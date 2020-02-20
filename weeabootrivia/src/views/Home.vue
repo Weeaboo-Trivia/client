5ef5de5d2174abbf46f2364e138cfe0c84526457
nafies1 <ghuroba.muslim@gmail.com>
2020-02-20 20:49:11 +0700

    create rooms, still on progress

--- weeabootrivia/src/views/Home.vue
+++ weeabootrivia/src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input @keyup.enter="addRoom" type="text" v-model="roomName">
+    <button @click="addRoom">Add room</button>
+    <div class="row mt-3">
+      <div class="col-sm-2" v-for="room in listRooms" :key="room.id">
+        <b-card bg-variant="light" :header="`Room ${room.id}`" class="text-center" @click="enterRoom(room.id)">
+          <b-card-text>Room {{ room.name }}</b-card-text>
+        </b-card>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import io from 'socket.io-client'
+const socket = io('http://localhost:3000')
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      roomName: '',
+      listRooms: []
+    }
+  },
+  methods: {
+    addRoom () {
+      console.log('masuk ke add room', this.roomName)
+      socket.emit('createRoom', this.roomName)
+    },
+    enterRoom (id) {
+      this.$router.push(`/room/${id}`)
+    }
+  },
+  created () {
+    socket.emit('fetchRooms', this.roomName)
+    socket.on('roomCreated', (room) => {
+      this.$bvToast.toast(`Add room ${room.id} success`, {
+        title: 'Add Room success',
+        variant: 'success',
+        solid: true
+      })
+      socket.emit('fetchRooms', this.roomName)
+    })
+    socket.on('showRooms', (rooms) => {
+      this.listRooms = rooms
+    })
   }
 }
 </script>
