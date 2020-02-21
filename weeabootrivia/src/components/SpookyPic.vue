5c7402db329f496a36d72f989965c9e76e81c58e
mcc-19 <mcc-19@mcc-19s-Mac-mini.local>
2020-02-21 10:34:45 +0700

    prepare for merge

---
+++ weeabootrivia/src/components/SpookyPic.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <b-button v-b-modal.jumpScare>Launch centered modal</b-button>
+  <b-modal id="jumpScare" centered hide-footer hide-header hide-body class="text-left">
+      <img style="max-height: 500px; max-width: 470px" src="https://i.pinimg.com/originals/5d/6b/78/5d6b78a6c628ac18fdd84768ad7d4fbf.jpg" alt="">
+  </b-modal>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
