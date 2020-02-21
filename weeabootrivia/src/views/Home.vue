b63d8724bbe06963fde8840ce7d8f131fd981d76
nafies1 <ghuroba.muslim@gmail.com>
2020-02-21 10:03:17 +0700

    merge development to createRooms. Start Game on progress

--- weeabootrivia/src/views/Home.vue
+++ weeabootrivia/src/views/Home.vue
@@ -1,12 +1,32 @@
 <template>
   <div class="home">
-    <input @keyup.enter="addRoom" type="text" v-model="roomName">
-    <button @click="addRoom">Add room</button>
-    <div class="row mt-3">
-      <div class="col-sm-2" v-for="room in listRooms" :key="room.id">
-        <b-card bg-variant="light" :header="`Room ${room.id}`" class="text-center" @click="enterRoom(room.id)">
-          <b-card-text>Room {{ room.name }}</b-card-text>
-        </b-card>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm text-center">
+          <br><br>
+          <input @keyup.enter="addRoom" class="" type="text" v-model="roomName" placeholder="room name">
+          <button @click="addRoom" class="btn btn-success btn-sm" style="margin-left: 5px;"><i class="fas fa-sign-in-alt"></i></button>
+          <br><br>
+          <div class="card card-signup z-depth-0 text-center bg-transparent border-0" style="width: 500px;height: 600px; max-height: 600px;">
+            <div class="card-header">
+              <img src="../assets/e56.gif" alt="" style="height: 150px">
+            </div>
+            <div class="card-body" style="overflow: auto;">
+              <div class="row mt-3">
+                <div class="col-sm-2" v-for="room in listRooms" :key="room.id">
+                  <b-card bg-variant="light" :header="`Room ${room.id}`" class="text-center" @click="enterRoom(room.id)">
+                    <b-card-text>Room {{ room.name }}</b-card-text>
+                  </b-card>
+                </div>
+              </div>
+            </div>
+          <audio src="../assets/sound/まったくなんだかなあ (MATTAKU NANNDAKA NAA) Saekano Original Sound Track.mp3" controls id="myKato" autoplay='autoplay' loop='loop' preload="auto" type='audio/mp3'></audio>
+          </div>
+        </div>
+        <div class="col-sm text-center">
+          <b-calendar locale="id"></b-calendar>
+        </div>
       </div>
     </div>
 
@@ -15,6 +35,7 @@
 
 <script>
 // @ is an alias to /src
+// import { Howl } from 'howler'
 
 export default {
   name: 'Home',
@@ -31,6 +52,18 @@ export default {
     },
     enterRoom (id) {
       this.$router.push(`/room/${id}`)
+    },
+    playSound () {
+      $('audio').play()
+      // var sound = new Howl({
+      //   src: ['../assets/sound/まったくなんだかなあ (MATTAKU NANNDAKA NAA) Saekano Original Sound Track.mp3'],
+      //   autoplay: true,
+      //   loop: true,
+      //   volume: 0.5,
+      //   onend: function () {
+      //     console.log('Finished!')
+    // })
+    // sound.play()
     }
   },
   created () {
@@ -46,11 +79,28 @@ export default {
     this.socket.on('showRooms', (rooms) => {
       this.listRooms = rooms
     })
+    this.playSound()
   },
   computed: {
     socket () {
       return this.$store.state.socket
     }
+  },
+  mounted () {
+    const x = document.getElementById('myKato')
+    x.autoplay = true
+    x.load()
   }
 }
 </script>
+
+<style >
+  .bg {
+    background-image: url('../assets/saenai-heroine-no-sodatekata-megumi-kato-uhd-4k-wallpaper.jpg');
+    max-height: 100vh;
+    /* background-position: center; */
+    overflow: hidden;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
--- weeabootrivia/src/views/Room.vue
+++ weeabootrivia/src/views/Room.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="about">
-    <h1>Room {{ roomKe }}</h1>
-    <label for="enterRoom">Enter your name to enter this room</label> <br>
-    <input id="enterRoom" @keyup.enter="submitName" type="text" v-model="name" placeholder="Enter your name and press enter..." style="width: 300px;">
-    <br><br>
-    <br>
-    {{ room }}
-    <button @click="startGame" class="btn btn-primary" v-if="startButton">Start Game</button>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4"></div>
+        <div class="col-sm-7">
+          <div class="card card-signup z-depth-0 bg-transparent border-0">
+            <div class="card-header text-center">
+              <h1 style="color: #8B008B">Room {{ roomKe }}</h1>
+              <label for="enterRoom">Enter your name to enter this room</label> <br>
+              <input id="enterRoom" @keyup.enter="submitName" type="text" v-model="name" placeholder="Enter your name and press enter..." style="width: 300px;">
+            </div>
+            <div class="card-body">
+              <b-card-group style="height: 500px; max-height: 500px">
+                <b-card style="background-color: #FAEBD7">
+                  <b-card-img src="https://i.ya-webdesign.com/images/anime-png-gifs-6.gif" class="rounded-0" style="margin-top: 80px; background-color: #FAEBD7"></b-card-img>
+                </b-card>
+                <b-card>
+                  {{ room }}
+                  <button class="btn btn-primary" v-if="startButton">Start Game</button>
+                </b-card>
+              </b-card-group>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-1"></div>
+      </div>
+    </div>
   </div>
 </template>
 
